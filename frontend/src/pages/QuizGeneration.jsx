8fb897d5c89e40667695f6a5857cbaa532b55ad2
juaancasillaa <juanacasilla16@outlook.com>
2024-08-20 15:54:36 -0400

    Final Commit

--- frontend/src/pages/QuizGeneration.jsx
+++ frontend/src/pages/QuizGeneration.jsx
@@ -21,7 +21,7 @@ const QuizGeneration = ({ setQuiz }) => {
     setLoading(true);
 
     try {
-      const response = await fetch("https://learner-backend-12y8im7ro-juaancasillaas-projects.vercel.app/generate-quiz", {
+      const response = await fetch("learner-backend.vercel.app/generate-quiz", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
